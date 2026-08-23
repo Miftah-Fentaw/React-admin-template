d0c61eec8a8a402bddc6a28a7803d382f1db1547
Miftah-Fentaw <miftahfentawdev@gmail.com>
2026-08-23 13:47:45 +0300

    chore(charts): export GroupedBarChart and zipToGrouped from charts barrel

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,4 +1,6 @@
 export { AreaChart } from './AreaChart'
 export { BarChart } from './BarChart'
+export { GroupedBarChart, zipToGrouped } from './GroupedBarChart'
+export type { GroupedSeriesPoint, GroupedBarChartProps } from './GroupedBarChart'
 export { DonutChart } from './DonutChart'
 export type { DonutSegment } from './DonutChart'
